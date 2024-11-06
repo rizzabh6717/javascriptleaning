60be56cf825f5ccaeb055ec03d29e7fd0bd32be8
Rishabh Chaudhary <csai22058@glbitm.ac.in>
2024-11-06 16:37:12 +0000

    math

---
+++ 01_basics/data.js
@@ -0,0 +1,35 @@
+let name="rishabh"
+let age=10
+let isokay=true
+//console.log(typeof"rishabh");
+//console.log(typeof age);
+let x=3
+let y=++x
+let z=y
+//console.log(x,y ,z)
+//console.log(x,y ,z)
+
+//console.log(1+2+"3");
+
+//console.log("2">1)   //type coersion
+//console.log(null)
+//console.log(typeof null)   //it is object
+
+//let hero=["jett","reyna","clove"];
+//console.log(typeof hero)               //it is object
+
+//let myobj={
+   // name: "rishabh",
+    //age: 10
+//}
+//console.log(typeof myobj)
+
+//console.log(name);
+
+//const gamename= new String('rishu')
+//console.log(gamename);
+//console.log(name.length);
+//console.log(name.toUpperCase());
+//console.log(name.slice (-7,4));            //output=rish
+//console.log(name.substring(0,4));         //output=rish
+//console.log(`my name is ${name} and my age is ${age}`)
