2f52e10f6a80f92216b1a97d5085911aab77a187
Rishabh Chaudhary <csai22058@glbitm.ac.in>
2024-11-18 11:48:53 +0000

    func

---
+++ 01_basics/functions.js
@@ -0,0 +1,44 @@
+// function sayMyName(){
+//     console.log("r");
+//     console.log("I");
+//     console.log("s");
+//     console.log("h");
+//     console.log("a");
+//     console.log("b");
+// }
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+//function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+   // return number1 + number2
+//}
+
+//const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+//function loginUserMessage(username = "sam"){
+//    if(!username){
+ //       console.log("PLease enter a username");
+//        return
+  //  }
+//    return `${username} just logged in`
+//}
+
+// console.log(loginUserMessage("rishabh"))
+// console.log(loginUserMessage("rishabh"))
+
+
+function calculate(num1){
+    return num1;
+}
+console.log(calculate(2));
